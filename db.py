3808040de4b7df51e22d9010d5a484b75d50abcb
JESSE MWENDWA NDUNDA <jessendunda2@gmail.com>
2025-08-28 22:40:25 +0300

    add data to the db file

---
+++ db.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "sqlite:///art_gallery.db"
+
+engine = create_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(bind=engine)
+
+Base = declarative_base()
+
+def get_session():
+    return SessionLocal()
